18063621e1a4f64b8d4764612f07c1dcd6d41cc7
Ope Omiwade <opeomiwade21@gmail.com>
2024-06-17 23:22:04 +0100

    marketplace page started - filter component ui complete, listings being displayed, category header component complete

---
+++ src/components/marketplace-page/MarketListing.tsx
@@ -0,0 +1,58 @@
+import { Listing } from "@/types/types";
+import ListingImage from "./ImageComponent";
+import { motion } from "framer-motion";
+import { MdOutlineBookmark } from "react-icons/md";
+import { MdBookmarkBorder } from "react-icons/md";
+import { useContext } from "react";
+import ListingContext from "@/context/ListingContext";
+
+const MarketListing: React.FC<{
+  listing: Listing;
+}> = ({ listing }) => {
+  const ctx = useContext(ListingContext);
+  return (
+    <div key={listing.listingId} className="flex flex-col gap-2">
+      <ListingImage images={listing.images} listingId={listing.listingId} />
+      <div className="w-full flex justify-between">
+        <p className="text-sm">{listing.listedProduct.productName}</p>
+        {ctx.savedListings &&
+        ctx.savedListings.some(
+          (savedListing) => savedListing.listingId === listing.listingId
+        ) ? (
+          <motion.button
+            whileTap={{ scale: 1.2 }}
+            transition={{
+              stiffness: 400,
+              duration: 0.6,
+              type: "spring",
+            }}
+            onClick={() => {
+              ctx.removeSavedListingHandler(listing.listingId);
+            }}
+          >
+            <MdOutlineBookmark size={25} />
+          </motion.button>
+        ) : (
+          <motion.button
+            whileTap={{ scale: 1.2 }}
+            transition={{
+              stiffness: 400,
+              duration: 0.6,
+              type: "spring",
+            }}
+            onClick={() => {
+              ctx.addSavedListingHandler(listing);
+            }}
+          >
+            <MdBookmarkBorder size={25} />
+          </motion.button>
+        )}
+      </div>
+      <p className="text-sm text-green-500 font-bold">
+        £{listing.price.toFixed(2)}
+      </p>
+    </div>
+  );
+};
+
+export default MarketListing;
--- src/components/marketplace-page/MarketPlaceGrid.tsx
+++ src/components/marketplace-page/MarketPlaceGrid.tsx
@@ -1,18 +1,15 @@
 "use client";
 import { useEffect, useState, useContext } from "react";
-import { motion } from "framer-motion";
 import toTitleCase from "@/utils/toTitleCase";
 import { useQuery } from "@tanstack/react-query";
 import { getListingByCategory } from "@/lib/actions";
 import { CircularProgress } from "@mui/material";
 import { DashDetails, Listing } from "@/types/types";
-import { MdOutlineBookmark } from "react-icons/md";
-import { MdBookmarkBorder } from "react-icons/md";
 import queryClient from "@/lib/http";
-import ListingImage from "./ImageComponent";
 import ListingContext from "@/context/ListingContext";
 import CategoryHeader from "./CategoryHeader";
 import FilterComponent from "./FilterComponent";
+import MarketListing from "./MarketListing";
 
 const MarketPlaceGrid = () => {
   const [selected, setSelected] = useState<string>("consoles");
@@ -59,55 +56,7 @@ const MarketPlaceGrid = () => {
         ) : (
           <div className="grid grid-cols-5 h-full w-full p-2 gap-5">
             {listings?.map((listing) => {
-              return (
-                <div key={listing.listingId} className="flex flex-col gap-2">
-                  <ListingImage
-                    images={listing.images}
-                    listingId={listing.listingId}
-                  />
-                  <div className="w-full flex justify-between">
-                    <p className="text-sm">
-                      {listing.listedProduct.productName}
-                    </p>
-                    {ctx.savedListings &&
-                    ctx.savedListings.some(
-                      (savedListing) =>
-                        savedListing.listingId === listing.listingId
-                    ) ? (
-                      <motion.button
-                        whileTap={{ scale: 1.2 }}
-                        transition={{
-                          stiffness: 400,
-                          duration: 0.6,
-                          type: "spring",
-                        }}
-                        onClick={() => {
-                          ctx.removeSavedListingHandler(listing.listingId);
-                        }}
-                      >
-                        <MdOutlineBookmark size={25} />
-                      </motion.button>
-                    ) : (
-                      <motion.button
-                        whileTap={{ scale: 1.2 }}
-                        transition={{
-                          stiffness: 400,
-                          duration: 0.6,
-                          type: "spring",
-                        }}
-                        onClick={() => {
-                          ctx.addSavedListingHandler(listing);
-                        }}
-                      >
-                        <MdBookmarkBorder size={25} />
-                      </motion.button>
-                    )}
-                  </div>
-                  <p className="text-sm text-green-500 font-bold">
-                    £{listing.price.toFixed(2)}
-                  </p>
-                </div>
-              );
+              return <MarketListing listing={listing} />;
             })}
           </div>
         )}
